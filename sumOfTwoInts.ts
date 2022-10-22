5388fca945538c337b16bed49a00edf98b38e04b
Ioan Latrovaliev <latrovalievioan@gmail.com>
2022-10-22 10:07:28 +0300

    implement sum of two positive ints

---
+++ sumOfTwoInts.ts
@@ -0,0 +1,22 @@
+type decToLength<
+  T extends number,
+  Arr extends any[] = []
+> = Arr['length'] extends T 
+  ? Arr 
+  : decToLength<T, [...Arr, '.']>;
+
+type sumOfTwoLengths<
+  L1 extends any[], 
+  L2 extends any[]
+> = [...L1, ...L2]['length'];
+
+type sumOfTwoPositiveInts<
+  N1 extends number, 
+  N2 extends number
+> = sumOfTwoLengths<
+  decToLength<N1>,
+  decToLength<N2>
+>;
+
+
+type x = sumOfTwoPositiveInts<999, 999>
